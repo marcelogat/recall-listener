8f9967e10bc18d7bad74aa33d39555c70d3a71a9
Marcelo Gatinsky <marcelogatinsky@gmail.com>
2025-11-14 17:35:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -144,10 +144,10 @@ wss.on('connection', function connection(ws, req) { // 'req' nos da la petición
                     }
                     
                     // Iniciar nuevo timeout (llama al processCompleteUtterance de esta conexión)
+                    // ESTA ES LA LÍNEA CORREGIDA:
                     timeoutId = setTimeout(() => {
-                        processCompleteUtterance(),
-                        SILENCE_TIMEOUT
-                    });
+                        processCompleteUtterance();
+                    }, SILENCE_TIMEOUT);
                 }
             } catch (e) {
                 // Si no es JSON, solo mostramos el mensaje
